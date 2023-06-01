eae21c1d643415685ecfe306588595a257da046a
goozyshi <39510459+goozyshi@users.noreply.github.com>
2023-06-02 04:42:45 +0800

    xx

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <div v-for="route in constantRoutes" :key="route">
+          <div>{{route.name}}</div>
+        </div>
+      </el-aside>
       <el-container>
         <layout-header></layout-header>
         <layout-content></layout-content>
@@ -14,4 +18,5 @@
   import LayoutHeader from "./header/Header.vue";
   import LayoutContent from "./content/Content.vue";
   import LayoutFooter from "./footer/Footer.vue";
+  import { constantRoutes } from "../router";
 </script>
